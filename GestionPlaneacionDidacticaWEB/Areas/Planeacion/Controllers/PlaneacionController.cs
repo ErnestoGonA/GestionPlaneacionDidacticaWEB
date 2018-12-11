6063c0b34ed3ed57d636565abedc5c4c687a343a
Juan Pedro Gil Llanos <35576920+juanpedrogil@users.noreply.github.com>
2018-12-11 13:23:29 -0700

    Planeacion lista

---
+++ GestionPlaneacionDidacticaWEB/Areas/Planeacion/Controllers/PlaneacionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionPlaneacionDidacticaWEB.Areas.Planeacion.Services;
+using GestionPlaneacionDidacticaWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Controllers
+{
+    [Area("Planeacion")]
+    public class PlaneacionController : Controller
+    {
+        FicSrvPlaneacion FicSrvPlaneacion;
+        List<eva_planeacion> FicListaPlaneacion;
+        eva_planeacion planeacion;
+        public PlaneacionController()
+        {
+            FicSrvPlaneacion = new FicSrvPlaneacion();
+        }
+        public IActionResult FicViPlaneacionList()
+        {
+            try
+            {
+                FicListaPlaneacion = FicSrvPlaneacion.FicGetListTemas().Result;
+                ViewBag.Title = "Catalogo de planeaciones";
+                return View(FicListaPlaneacion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ GestionPlaneacionDidacticaWEB/Areas/Planeacion/Services/FicSrvPlaneacion.cs
@@ -0,0 +1,34 @@
+﻿using GestionPlaneacionDidacticaWEB.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Services
+{
+    public class FicSrvPlaneacion
+    {
+
+        HttpClient client;
+        public FicSrvPlaneacion()
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri("http://localhost:53483/");
+            this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<List<eva_planeacion>> FicGetListTemas()
+        {
+            HttpResponseMessage FicResponse = await this.client.GetAsync("api/Planeaciones");
+            if (FicResponse.IsSuccessStatusCode)
+            {
+                var FicRespuesta = await FicResponse.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<eva_planeacion>>(FicRespuesta);
+            }
+            //return null;
+            return new List<eva_planeacion>();
+        }
+    }
+}
