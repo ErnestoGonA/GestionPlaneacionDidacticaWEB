390ad900b1114c2a6ace3d2bb9a9cf41cdc5fcc4
ernesto gonzalez arellano <ergonzalezar@ittepic.edu.mx>
2018-12-16 16:53:05 -0700

    LISTA DE COMPETENCIAS

---
+++ GestionPlaneacionDidacticaWEB/Areas/Planeacion/Controllers/CompetenciasController.cs
@@ -0,0 +1,40 @@
+﻿using GestionPlaneacionDidacticaWEB.Areas.Planeacion.Services;
+using GestionPlaneacionDidacticaWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Controllers
+{
+    [Area("Planeacion")]
+    public class CompetenciasController : Controller
+    {
+        FicSrvCompetencias FicSrvCompetencias;
+
+        List<eva_planeacion_temas_competencias> FicListaCompetencias;
+        eva_planeacion_temas_competencias Competencia;
+
+        public CompetenciasController()
+        {
+            FicSrvCompetencias = new FicSrvCompetencias();
+        }
+
+
+
+        public IActionResult FicViCompetenciasList(short IdAsignatura, int IdPlaneacion, short IdTema)
+        {
+            try
+            {
+                FicListaCompetencias = FicSrvCompetencias.FicGetListCompetencias(IdAsignatura,IdPlaneacion, IdTema).Result;
+                ViewBag.Title = "Catalogo de Competencias";
+                return View(FicListaCompetencias);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
