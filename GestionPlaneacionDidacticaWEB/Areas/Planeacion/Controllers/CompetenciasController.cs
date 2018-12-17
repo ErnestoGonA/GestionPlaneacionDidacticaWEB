a7aeac8f25bde895363433eb7ca49771c0a4b77a
ernesto gonzalez arellano <ergonzalezar@ittepic.edu.mx>
2018-12-16 19:18:37 -0700

    ADD competencias

--- GestionPlaneacionDidacticaWEB/Areas/Planeacion/Controllers/CompetenciasController.cs
+++ GestionPlaneacionDidacticaWEB/Areas/Planeacion/Controllers/CompetenciasController.cs
@@ -1,6 +1,8 @@
 ﻿using GestionPlaneacionDidacticaWEB.Areas.Planeacion.Services;
 using GestionPlaneacionDidacticaWEB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +23,13 @@ namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Controllers
             FicSrvCompetencias = new FicSrvCompetencias();
         }
 
-
-
         public IActionResult FicViCompetenciasList(short IdAsignatura, int IdPlaneacion, short IdTema)
         {
             try
             {
+                ViewBag.IdPlaneacion = IdPlaneacion;
+                ViewBag.IdAsignatura = IdAsignatura;
+                ViewBag.IdTema = IdTema;
                 FicListaCompetencias = FicSrvCompetencias.FicGetListCompetencias(IdAsignatura,IdPlaneacion, IdTema).Result;
                 ViewBag.Title = "Catalogo de Competencias";
                 return View(FicListaCompetencias);
@@ -35,6 +38,36 @@ namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Controllers
             {
                 throw e;
             }
+        }
+
+        public IActionResult FicViCompetenciasCreate(short asignatura, short planeacion, short tema)
+        {
+            var Competencia = new eva_planeacion_temas_competencias();
+            ViewBag.Competencias = new SelectList(new List<SelectListItem>(), "Value", "Text");
+            Competencia.IdPlaneacion = planeacion;
+            Competencia.IdAsignatura = asignatura;
+            Competencia.IdTema = tema;
+            return View(Competencia);
+        }
+
+        [HttpPost]
+        public ActionResult FicViCompetenciasCreate(eva_planeacion_temas_competencias FicCompetencia)
+        {
+            FicCompetencia.IdCompetencia = Convert.ToInt16(Request.Form["Competencias"].ToString());
+            Competencia = FicSrvCompetencias.GetCompetencia(FicCompetencia.IdAsignatura, FicCompetencia.IdPlaneacion, FicCompetencia.IdTema, FicCompetencia.IdCompetencia).Result;
+            if (Competencia.IdCompetencia == -1)
+            {
+                FicCompetencia.IdCompetencia = Convert.ToInt16(Request.Form["Competencias"].ToString());
+                FicCompetencia.FechaReg = DateTime.Now;
+                FicCompetencia.FechaUltMod = DateTime.Now;
+                FicCompetencia.UsuarioReg = "Ernesto";
+                FicCompetencia.UsuarioUltMod = "Ernesto";
+                FicCompetencia.Activo = "S";
+                FicCompetencia.Borrado = "N";
+                FicSrvCompetencias.CreateCompetencia(FicCompetencia).Wait();
+                return RedirectToAction("FicViCompetenciasList", new { FicCompetencia.IdPlaneacion, FicCompetencia.IdAsignatura, FicCompetencia.IdTema });
+            }
+            return RedirectToAction("FicViCompetenciasList", new { FicCompetencia.IdPlaneacion, FicCompetencia.IdAsignatura, FicCompetencia.IdTema });
         }
     }
 }
--- GestionPlaneacionDidacticaWEB/Areas/Planeacion/Services/FicSrvCompetencias.cs
+++ GestionPlaneacionDidacticaWEB/Areas/Planeacion/Services/FicSrvCompetencias.cs
@@ -33,5 +33,32 @@ namespace GestionPlaneacionDidacticaWEB.Areas.Planeacion.Services
             //return null;
             return new List<eva_planeacion_temas_competencias>();
         }
+
+        public async Task<eva_planeacion_temas_competencias> GetCompetencia(short idAsignatura, int idPlaneacion, short idTema, int idCompetencia)
+        {
+            HttpResponseMessage FicResponse = await this.client.GetAsync("api/Asignatura/" + idAsignatura + "/Planeacion/" + idPlaneacion + "/temas/" + idTema+"/competencias/"+idCompetencia);
+            if (FicResponse.IsSuccessStatusCode)
+            {
+                var FicRespuesta = await FicResponse.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<eva_planeacion_temas_competencias>(FicRespuesta);
+            }
+            //return null;
+            return new eva_planeacion_temas_competencias();
+        }
+
+        public async Task<eva_planeacion_temas_competencias> CreateCompetencia(eva_planeacion_temas_competencias competencia)
+        {
+            var json = JsonConvert.SerializeObject(competencia);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var respuestaPost = await client.PostAsync("api/competencia", content);
+            if (respuestaPost.IsSuccessStatusCode)
+            {
+
+                return competencia;
+            }
+            return null;
+        }
+
+
     }
 }
